d5ddc7218196144f2a4edd5964291e8cd8ec73e6
Harry <127743950+thorfinn22@users.noreply.github.com>
2024-04-24 12:07:59 +1000

    Add files via upload

---
+++ 11.1/Program.py
@@ -0,0 +1,69 @@
+import os
+from time import sleep
+
+
+class Counter:
+    def __init__(self, name):
+        self._count = 0
+        self._name = name
+
+    def Increment(self):
+        self._count += 1
+
+    def Reset(self):
+        self._count = 0
+
+    @property
+    def Name(self):
+        return self._name
+
+    @Name.setter
+    def Name(self, value):
+        self._name = value
+
+    @property
+    def Ticks(self):
+        return self._count
+
+
+class Clock:
+    def __init__(self):
+        self._seconds = Counter("seconds")
+        self._minutes = Counter("minutes")
+        self._hours = Counter("hours")
+
+    def Tick(self):
+        self._seconds.Increment()
+        if self._seconds.Ticks > 59:
+            self._minutes.Increment()
+            self._seconds.Reset()
+            if self._minutes.Ticks > 59:
+                self._hours.Increment()
+                self._minutes.Reset()
+                if self._hours.Ticks > 23:
+                    self.Reset()
+
+    def Reset(self):
+        self._seconds.Reset()
+        self._minutes.Reset()
+        self._hours.Reset()
+
+    @property
+    def Time(self):
+        width = 2
+        return (
+            str(self._hours.Ticks).zfill(width)
+            + ":"
+            + str(self._minutes.Ticks).zfill(width)
+            + ":"
+            + str(self._seconds.Ticks).zfill(width)
+        )
+
+
+clock = Clock()
+
+for x in range(86400):
+    clock.Tick()
+    print(clock.Time)
+    sleep(1)
+    os.system('clear')
